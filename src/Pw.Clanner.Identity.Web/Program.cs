bf85a7b8c97ab237f41c9d19ec8b2f348574ff0e
MeowExort <tobilts@yandex.ru>
2025-06-13 02:09:54 +0300

    Добавляет метрики и трейсинг

--- src/Pw.Clanner.Identity.Web/Program.cs
+++ src/Pw.Clanner.Identity.Web/Program.cs
@@ -1,4 +1,7 @@
 using System.Text.Json.Serialization;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateSlimBuilder(args);
 
@@ -6,6 +9,36 @@ builder.Services.ConfigureHttpJsonOptions(options =>
 {
     options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
 });
+
+
+var useOtlp = builder.Configuration.GetValue<bool>("App:Jaeger:Enabled");
+
+if (useOtlp)
+{
+    builder.Services.AddOpenTelemetry()
+        .WithTracing(opts => opts
+            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("identity"))
+            .AddAspNetCoreInstrumentation(options =>
+            {
+                options.RecordException = true;
+                options.Filter = context => context.Request.Path.Value?.Contains("/health") == false;
+            })
+            .AddHttpClientInstrumentation(options =>
+            {
+                options.RecordException = true;
+                options.FilterHttpWebRequest = request =>
+                    request.RequestUri.PathAndQuery.Contains("/health") == false;
+                options.FilterHttpRequestMessage = message =>
+                    message.RequestUri?.PathAndQuery.Contains("/health") == false;
+            })
+            .AddOtlpExporter(o => { o.Endpoint = new Uri(builder.Configuration["Otlp:Endpoint"]!); })
+        )
+        .WithMetrics(opts => opts
+            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("identity"))
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddOtlpExporter(o => { o.Endpoint = new Uri(builder.Configuration["Otlp:Endpoint"]!); }));
+}
 
 var app = builder.Build();
 
