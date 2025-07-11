81d26353f1e8a509ef9c6bf98a68b7ec62b66d31
MeowExort <tobilts@yandex.ru>
2025-07-12 02:28:42 +0300

    logs

--- src/Pw.Clanner.Identity/Features/Hydra/LoginHydraQueryController.cs
+++ src/Pw.Clanner.Identity/Features/Hydra/LoginHydraQueryController.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Pw.Clanner.Identity.Common;
 using Pw.Clanner.Identity.Common.Interfaces;
-using Pw.Clanner.Identity.Common.Security;
 
 namespace Pw.Clanner.Identity.Features.Hydra;
 
-[HydraChallenge(false, true)]
-public class LoginHydraQueryController(ICurrentHydraChallenge hydraChallenge) : ApiControllerBase
+public class LoginHydraQueryController(ICurrentHydraChallenge hydraChallenge, ILogger<LoginHydraQueryController> logger)
+    : ApiControllerBase
 {
     [HttpGet("/hydra/login")]
     public async Task<ActionResult> GetUserAudit()
     {
+        var t = HttpContext?.Request?.Query["login_challenge"];
+        logger.LogInformation("HYDRA REQUEST {LoginChallenge}", hydraChallenge.LoginChallenge);
+        logger.LogInformation("HYDRA REQUEST2 {LoginChallenge}", t);
         hydraChallenge.StoreLoginChallenge();
         return Redirect("https://auth.clanner.pw");
     }
