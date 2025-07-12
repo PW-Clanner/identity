a8d140f046717d09b51486009b392ba6facc6c4f
MeowExort <tobilts@yandex.ru>
2025-07-12 05:03:28 +0300

    Добавляет hydra consent

---
+++ src/Pw.Clanner.Identity/Features/Hydra/ConsentHydraQueryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ory.Hydra.Client.Api;
+using Pw.Clanner.Identity.Common;
+using Pw.Clanner.Identity.Common.Interfaces;
+
+namespace Pw.Clanner.Identity.Features.Hydra;
+
+public class ConsentHydraQueryController(ICurrentHydraChallenge hydraChallenge, IOAuth2ApiAsync oauth2Api)
+    : ApiControllerBase
+{
+    [HttpGet("/hydra/consent")]
+    public async Task<ActionResult> ConsentHydra(ConsentHydraQuery query)
+    {
+        hydraChallenge.ConsentChallenge = query.ConsentChallenge;
+
+        var acceptResponse = await oauth2Api.AcceptOAuth2ConsentRequestAsync(hydraChallenge.ConsentChallenge);
+
+        return Redirect(acceptResponse.RedirectTo);
+    }
+}
